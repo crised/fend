2d696901d6eb1450bf16d668d3d47492b81a32d3
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-29 15:26:09 -0300

    Added jest tests, both passing with yarn jest

---
+++ projects/evaluate-news-nlp/__test__/testCheckForName.spec.js
@@ -0,0 +1,7 @@
+import {checkForName} from "../src/client/js/nameChecker";
+
+describe("Testing the checkForName functionality", () => {
+    test("Testing the handleSubmit() function", () => {
+        expect(checkForName).toBeDefined();
+    })
+});
