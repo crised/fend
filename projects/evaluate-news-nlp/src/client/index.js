d4dbfa32bc46fb2f59c0659103f69c6c62104c28
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-29 16:32:08 -0300

    css showing correctly

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,14 +1,19 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-import './styles/resets.scss'
 
 console.log(checkForName);
-console.log(handleSubmit());
+console.log(handleSubmit);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+export {
+    checkForName,
+    handleSubmit
+}
